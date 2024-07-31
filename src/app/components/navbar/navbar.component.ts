930226ebc8ef12997521d1340712757f0c3dfc69
Eslam Ashraf <71986226+EslamAsHhraf@users.noreply.github.com>
2024-07-31 20:17:23 +0300

    made navbar fixed

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
   ngOnInit(): void {
+    this.handleScroll();
   }
   active: string = 'Home';
   toggle: boolean = false;
+  scrolled: boolean = false;
 
   navLinks = [
     {
@@ -40,5 +42,19 @@ export class NavbarComponent implements OnInit {
 
   toggleMenu(): void {
     this.toggle = !this.toggle;
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.handleScroll();
+  }
+
+  private handleScroll() {
+    const scrollTop = window.scrollY || document.documentElement.scrollTop;
+    if (scrollTop > 100) {
+      this.scrolled = true;
+    } else {
+      this.scrolled = false;
+    }
   }
 }
